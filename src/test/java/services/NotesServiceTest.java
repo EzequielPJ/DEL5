381223cbeec9ce72d4240e4549ad3ede90747d9e
Angel Delgado <angeldelgadoluna@gmail.com>
2019-05-25 19:50:12 +0200

    Test de Note Completo. Servicio de Note arreglado.
    Colocado triple rol dentro del metodo save.

--- src/test/java/services/NotesServiceTest.java
+++ src/test/java/services/NotesServiceTest.java
@@ -47,9 +47,9 @@ public class NotesServiceTest extends AbstractTest {
 	public void driver() {
 		final Object[][] testingData = {
 			{
-				"", 0, super.getEntityId("event1"), null
+				"student1", 0, super.getEntityId("event1"), null
 			}, {
-				"", super.getEntityId("note2"), 0, IllegalArgumentException.class
+				"admin1", super.getEntityId("note2"), 0, IllegalArgumentException.class
 			}
 		};
 
